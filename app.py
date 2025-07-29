c61580199395f619e87292fd5aab13622783e373
monk1982 <hoangthuc2013@gmail.com>
2025-07-29 20:33:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,10 @@ if 'cam_on' not in ss:
 def main():
     pages = {
         "Chụp Ảnh": [
-            st.Page("gemini_ai.py", title="Đặt câu hỏi cho bức ảnh của bạn"),
-            #st.Page("manage_account.py", title="Manage your account"),
+            st.Page("gemini_ai.py", title="Đặt câu hỏi cho bức ảnh của bạn"),           
         ],
         "Fix Camera": [
-            st.Page("fix_camera.py", title="Thử nghiệm code"),
-            #st.Page("opencv_cam.py", title="Thử nghiệm OpenCV"),
+            st.Page("fix_camera.py", title="Thử nghiệm code"),            
         ],
     }
 
